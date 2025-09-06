a8254ccfc57c3db4c1c7c87f30020d8ce9d25ed7
jeancdev <jcode2006@gmail.com>
2025-09-06 13:02:15 -0500

    05: authentication socials

---
+++ src/modules/home/ui/views/home-view.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { authClient } from '@/lib/auth-client'
+
+import { Button } from '@/components/ui/button'
+
+const HomeView = () => {
+  const router = useRouter()
+
+  const { data: session } = authClient.useSession()
+
+  return (
+    <div>
+      <div>Hello {session?.user.name}</div>
+
+      <Button
+        onClick={() =>
+          authClient.signOut({
+            fetchOptions: {
+              onSuccess: () => {
+                router.push('/sign-in')
+              }
+            }
+          })
+        }
+      >
+        Sign out
+      </Button>
+    </div>
+  )
+}
+
+export { HomeView }
